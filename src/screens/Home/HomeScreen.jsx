9430efac466db9867000859024f518662d23eace
raihanmakarim <raihanmakarim21@gmail.com>
2023-01-23 06:25:36 +0700

    add root color pallete & landing page style changes

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./HomeScreen.css";
-import NavBar from "../../components/Navbar/NavBar";
-import geolokaLogo from "../../images/GeolokaLogo.png";
+import landingpagelogo from "../../images/landingpagelogo.png";
 import constant from "../../constant/descriptions.json";
 
 const HomeScreen = () => {
   return (
     <div className="home-screen-container">
       <div className="logo-title-desc-container">
-        <img src={geolokaLogo}></img>
         <div className="title-desc-container">
-          <div className="title-container">
-            <h1>GEOLOKA</h1>
-          </div>
-          <div className="desc-container">
+          <div className="title-container">GEOLOKA</div>
+          <div className="desc-container" style={{ maxWidth: "400px" }}>
             <p>{constant.homeDescription}</p>
           </div>
+          <img src={landingpagelogo}></img>
+        </div>
+        <div className="facts-wrapper">
+          <div className="facts-title-container">
+            <div className="facts-title">What is Urban Heat Island</div>
+          </div>
+          <div className="desc-container">asdasdsaqweqweqweqweqweqweqwe</div>
         </div>
       </div>
     </div>
