84b146d9145c473d096d705a0bcfd9f82abac14c
raihanmakarim <raihanmakarim21@gmail.com>
2023-01-24 21:03:21 +0700

    detailpage
    style changes

--- src/screens/DetailPage/DetailPage.jsx
+++ src/screens/DetailPage/DetailPage.jsx
@@ -3,10 +3,19 @@ import urban from "../../images/urban.png";
 import urban2 from "../../images/urban2.png";
 import "./DetailPage.css";
 import constant from "../../constant/descriptions.json";
+import spark from "../../images/spark.png";
+import cane from "../../images/cane.png";
+import pills from "../../images/pills.png";
+import logogeoloka3 from "../../images/logogeoloka3.png";
 
 export default function DetailPage() {
   return (
     <div>
+      <div></div>
+      <img src={pills} alt="" className="spark" />
+      <img src={cane} alt="" className="cane" />
+      <img src={spark} alt="" className="pills" />
+      <div className="bar"></div>
       <div className="top-section-container">
         <div>What is Urban Heat Island?</div>
         <img src={urban} alt="urban" />
@@ -14,7 +23,8 @@ export default function DetailPage() {
         <p>{constant.homeDescription}</p>
       </div>
       <div className="section-container">
-        <img src={urban2} alt="" />
+        <img src={logogeoloka3} alt="" className="logo3" />
+        <img src={urban2} alt="" className="urban2" />
         <div className="desc-text-container">
           <div className="urban2-title">What is Urban Heat Island?</div>
           <p>
@@ -25,6 +35,20 @@ export default function DetailPage() {
             absorb and retain heat.
           </p>
         </div>
+      </div>
+      <div className="section-container">
+        <img src={logogeoloka3} alt="" className="logo3" />
+        <h1>
+          What Caused <br></br>Urban Heat Island?
+        </h1>
+        <div></div>
+      </div>
+      <div className="section-container">
+        <img src={logogeoloka3} alt="" className="logo3" />
+        <h1>
+          What are the Impacts <br /> of Urban Heat Island?
+        </h1>
+        <div></div>
       </div>
     </div>
   );
