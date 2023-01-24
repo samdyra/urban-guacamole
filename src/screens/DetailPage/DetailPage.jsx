3db6af7eb2705c5a5055ff756d0fde711127b095
raihanmakarim <raihanmakarim21@gmail.com>
2023-01-25 03:20:59 +0700

    detailpage
    DetailPage finishe

--- src/screens/DetailPage/DetailPage.jsx
+++ src/screens/DetailPage/DetailPage.jsx
@@ -8,15 +8,31 @@ import cane from "../../images/cane.png";
 import pills from "../../images/pills.png";
 import logogeoloka3 from "../../images/logogeoloka3.png";
 
+const impact = [
+  {
+    title: "Increased Energy Consumption",
+    desc: "Heat islands increase the demand for air conditioning to cool buildings which also elevates overall electricity demand.",
+    img: pills,
+  },
+  {
+    title: "Compromised  Human Health & Comfort",
+    desc: "Heat islands contribute to heat-related deaths and illnesses such as general discomfort, respiratory difficulties, heat cramps, and non-fatal heat stroke.",
+    img: spark,
+  },
+  {
+    title: "Upraised Emmission of Air Pollutant",
+    desc: "Pollutants contribute to complex air quality problems such as the formation of ground-level ozone (smog), fine particulate matter, and acid rain.",
+    img: cane,
+  },
+];
+
 export default function DetailPage() {
   return (
     <div>
       <div></div>
-      <img src={pills} alt="" className="spark" />
-      <img src={cane} alt="" className="cane" />
-      <img src={spark} alt="" className="pills" />
-      <div className="bar"></div>
+
       <div className="top-section-container">
+        <div className="bar"></div>
         <div>What is Urban Heat Island?</div>
         <img src={urban} alt="urban" />
         <h1>GEOLOKA</h1>
@@ -28,27 +44,42 @@ export default function DetailPage() {
         <div className="desc-text-container">
           <div className="urban2-title">What is Urban Heat Island?</div>
           <p>
-            Urban Heat Island (UHI) is a heat concentration phenomenon in urban
-            areas due to an undistributed heat surface to the surrounding rural
-            areas. UHI occurs when cities replace natural land cover with dense
-            concentrations of pavement, buildings, and other surfaces that
-            absorb and retain heat.
+            Geoloka is a dynamic WebGIS that are expected to be used to mitigate
+            the negative effects of UHI on the environment and can be used as
+            the basis for monitoring UHI in urban strategic planning, urban
+            temperature control, and input for government policies related to
+            the environment in the future.
           </p>
         </div>
       </div>
-      <div className="section-container">
+      <div className="section2-container">
         <img src={logogeoloka3} alt="" className="logo3" />
         <h1>
           What Caused <br></br>Urban Heat Island?
         </h1>
         <div></div>
       </div>
-      <div className="section-container">
+      <div className="section2-container">
+        <img src={pills} alt="" className="spark" />
+        <img src={cane} alt="" className="cane" />
+        <img src={spark} alt="" className="pills" />
         <img src={logogeoloka3} alt="" className="logo3" />
         <h1>
           What are the Impacts <br /> of Urban Heat Island?
         </h1>
-        <div></div>
+        <div className="impact_container">
+          {impact.map((item, index) => {
+            return (
+              <div className="impact_box" id={index}>
+                <div className="flex">
+                  <img src={item.img} alt="" />
+                  <h1>{item.title}</h1>
+                </div>
+                <p>{item.desc}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
