9acac62fdceb69eda73cce81524a16ef8b177fdc
raihanmakarim <raihanmakarim21@gmail.com>
2023-01-25 23:14:08 +0700

    navbar
    add mobile navbar styling

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import geolokaLogo from "../../images/GeolokaLogo.png";
 import "./NavBarStyle.css";
 import { useLocation } from "react-router-dom";
@@ -6,13 +6,55 @@ const NavBar = () => {
   const location = useLocation();
   const isForm = location.pathname.includes("Form");
 
+  //hamburfer menu
+  const [burger_class, setBurgerClass] = useState("burgerbar");
+  const [menu_class, setMenuClass] = useState("menuHidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  // toggle burger menu change
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burgerbar2");
+      setMenuClass("menuVisible");
+    } else {
+      setBurgerClass("burgerbarunclicked");
+      setMenuClass("menuHidden");
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
+
+  const ScreenWidth = window.innerWidth;
+  const isMobile = ScreenWidth <= 500;
+
   return (
     <div className="nav-container">
       <div className="nav-element">
         <div className="geoloka-logo-container">
           <img src={geolokaLogo} className="geoloka-logo"></img>
         </div>
-        <div className="link-container">
+        {!isMobile ? (
+          <div className="link-container">
+            <a href="/">Home</a>
+            <a href="/UHIScreen">UHI Map</a>
+            {/* <a href="/RTRWScreen">RDTR Zoning Map</a> */}
+            <a href="/Chart">Statistics</a>
+            <a href="/NetizenScreen">People Contribution</a>
+            <a href="/Login">Login</a>
+            <a href="/FormScreen">Form</a>
+            <a href="/DetailPage">DetailPage</a>
+          </div>
+        ) : (
+          <div className="nav">
+            <div className="burgermenu" onClick={updateMenu}>
+              <div className={burger_class}></div>
+              <div className={burger_class}></div>
+              <div className={burger_class}></div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <div className={menu_class} onClick={() => setIsMenuClicked(true)}>
           <a href="/">Home</a>
           <a href="/UHIScreen">UHI Map</a>
           {/* <a href="/RTRWScreen">RDTR Zoning Map</a> */}
